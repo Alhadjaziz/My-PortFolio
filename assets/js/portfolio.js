396e59b9b5609d0173f08c01d43dd73e8c8ee1aa
Alhadjaziz <afsi_aziz@hotmail.fr>
2023-11-20 16:08:51 +0100

    first commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,10 @@
+let btns = document.querySelectorAll("btn");
+
+btns.forEach(function(buttons){
+    buttons.addventListener("click")
+    
+} )
+
+function downlodeBtn(){
+    alert("CV Downloded")
+}
